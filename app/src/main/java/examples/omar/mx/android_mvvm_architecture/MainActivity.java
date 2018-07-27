53927ed433dc48ce0f8280833973ab6eb81012fe
OmarGtz <Administrador@MacBook-Pro-de-Administrador.local>
2018-07-27 16:49:01 -0500

    firts commit

---
+++ app/src/main/java/examples/omar/mx/android_mvvm_architecture/MainActivity.java
@@ -0,0 +1,13 @@
+package examples.omar.mx.android_mvvm_architecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
